86a9ffc0ec304be55ae64e1dad9d5fa246936653
William <158026716+wgb8204@users.noreply.github.com>
2024-11-17 14:50:51 -0500

    Working RGB LED BLINKING PROGRAM

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,29 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
+#define RGB_PIN 48
 
-#define RGB_PIN 18
+Adafruit_NeoPixel pixel(1, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
-
-Adafruit_NeoPixel pixel(1, RGB_PIN, NEO_GRB+NEO_KHZ800);
-// put function declarations here:
 void colorCycle();
 
 void setup() {
-  pixel.begin();
-  pixel.clear();
+  pixel.begin();              // Initialize NeoPixel library
+  pixel.clear();              // Clear all pixels
+  pixel.setBrightness(255);   // Set brightness (0-255)
+  randomSeed(analogRead(A0)); // Seed random generator
 }
 
 void loop() {
-  colorCycle();
-  pixel.show();
-  delay(200);
+  colorCycle();               // Change colors
+  pixel.show();               // Update NeoPixel
+  delay(200);                 // Wait before next change
 }
 
-// put function definitions here:
 void colorCycle() {
-  int r = rand() % 255 +1;
-  int g = rand() % 255 +1;
-  int b = rand() % 255 +1;
+  int r = random(0, 256);     // Generate random red value
+  int g = random(0, 256);     // Generate random green value
+  int b = random(0, 256);     // Generate random blue value
 
-  pixel.setPixelColor(0, pixel.Color(255, 0, 0));  
+  pixel.setPixelColor(0, r, g, b); // Set pixel color
 }
